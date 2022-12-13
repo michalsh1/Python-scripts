3101326b7e8be82e4b71731ba04787f3234f6639
michalsh1 <michalshemesh1@gmail.com>
2022-12-13 19:15:53 +0200

    organizing and finishing bringing cotacts to crawl; organizing code

--- get_turtles_db.py
+++ get_turtles_db.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 import pyodbc
-import pandas
+import pandas as pd
 import numpy as np
 
 def connect(db_path):
@@ -36,122 +36,135 @@ query_TurtleEvent = "SELECT * FROM TurtleEvent"
 query_Regions = "SELECT * FROM Regions"
 
 
-df_AcCrawl = pandas.read_sql(query_AcCrawl, connection)
-df_AcHatch = pandas.read_sql(query_AcHatch, connection)
-df_Activities = pandas.read_sql(query_Activities, connection)
-df_Clutches = pandas.read_sql(query_Clutches, connection)
-df_ClutchToNest = pandas.read_sql(query_ClutchToNest, connection)
-df_Contact = pandas.read_sql(query_Contact, connection)
-df_ContactPosition = pandas.read_sql(query_ContactPosition, connection)
-df_Crawl = pandas.read_sql(query_Crawl, connection)
-df_CrawlContact = pandas.read_sql(query_CrawlContact, connection)
-df_CrawlPredator = pandas.read_sql(query_CrawlPredator, connection)
-df_Hatcheries = pandas.read_sql(query_Hatcheries, connection)
-df_Immerging = pandas.read_sql(query_Immerging, connection)
-df_Location = pandas.read_sql(query_Location, connection)
-df_Nest = pandas.read_sql(query_Nest, connection)
-df_Organization = pandas.read_sql(query_Organization, connection)
-df_Specie = pandas.read_sql(query_Specie, connection)
-df_TurtleEvent = pandas.read_sql(query_TurtleEvent, connection)
-df_Regions = pandas.read_sql(query_Regions, connection)
+df_AcCrawl = pd.read_sql(query_AcCrawl, connection)
+df_AcHatch = pd.read_sql(query_AcHatch, connection)
+df_Activities = pd.read_sql(query_Activities, connection)
+df_Clutches = pd.read_sql(query_Clutches, connection)
+df_ClutchToNest = pd.read_sql(query_ClutchToNest, connection)
+df_Contact = pd.read_sql(query_Contact, connection)
+df_ContactPosition = pd.read_sql(query_ContactPosition, connection)
+df_Crawl = pd.read_sql(query_Crawl, connection)
+df_CrawlContact = pd.read_sql(query_CrawlContact, connection)
+df_CrawlPredator = pd.read_sql(query_CrawlPredator, connection)
+df_Hatcheries = pd.read_sql(query_Hatcheries, connection)
+df_Immerging = pd.read_sql(query_Immerging, connection)
+df_Location = pd.read_sql(query_Location, connection)
+df_Nest = pd.read_sql(query_Nest, connection)
+df_Organization = pd.read_sql(query_Organization, connection)
+df_Specie = pd.read_sql(query_Specie, connection)
+df_TurtleEvent = pd.read_sql(query_TurtleEvent, connection)
+df_Regions = pd.read_sql(query_Regions, connection)
 
 
 ### contact df:
+
+### --- Generate FullName column without null values.
 # df_Contact['FullName']= df_Contact['ContactFname'] + ' ' + df_Contact['ContactLname']
-# df_Contact['FullNameTRY']= df_Contact['ContactFname'].astype(str) + ' ' + df_Contact['ContactLname'].astype(str)
 df_Contact['FullName']= df_Contact['ContactFname'].fillna('') + ' ' + df_Contact['ContactLname'].fillna('')
 
-df_Contacts_clean = df_Contact.drop(['ContactFname',
-                                     'ContactLname',
-                                     'ContactPhoto',
-                                     'ContactShirt',
-                                     'ContactActive',
-                                     'SyprusCoures',
-                                     'ContactBirthDate',
-                                     'ContactStreet',
-                                     'ContactHouseNum',
-                                     'ContactPostNum',
-                                     'ContactCity',
-                                     'ContactZipCode',
-                                     'ContactCountry',
-                                     'ContactSite',
-                                     'MailActive',
-                                     'MailPreference',
-                                     'CotactAddingDate',
-                                     'VolunteerGuided',
-                                     'VolunteerInstruction',
-                                     'VolunteerForms',
-                                     'VolunteerInsurance',
-                                     'ContactOwner',
-                                     'ContactOwnerCode'], axis=1)
-df_ContactPosition_Organization = pandas.merge(df_ContactPosition, df_Organization, left_on='Organization', right_on='OrganizationID', how='left')
-df_Contacts_Position_Organization = pandas.merge(df_Contacts_clean, df_ContactPosition_Organization, left_on='ContactPosition', right_on='PositionID', how='left')
-print(df_Contact.shape[0]==df_Contacts_Position_Organization.shape[0])
+df_Contacts_clean = df_Contact[['ContactId', 'ContactIDNum', 'ContactPosition', 'BeachSurveyor',
+       'NestRelocator', 'NestExcavator', 'ContactGender', 'ContactE-mail',
+       'ContactNotes','FullName']]
+
+### test- check for nan FullNames:
+if df_Contacts_clean['FullName'].isnull().values.any():
+    print ('there are null values!')
+
+# for n in range(0,len(df_Contacts_clean)):
+#     print(n, df_Contacts_clean['FullName'][n],df_Contacts_clean['ContactId'][n])
+
+df_ContactPosition_Organization = pd.merge(df_ContactPosition, df_Organization, left_on='Organization', right_on='OrganizationID', how='left')
+df_Contacts_Position_Organization = pd.merge(df_Contacts_clean, df_ContactPosition_Organization, left_on='ContactPosition', right_on='PositionID', how='left')
+# print(df_Contact.shape[0] == df_Contacts_Position_Organization.shape[0])
 
 
 ### location df
 df_Regions_clean = df_Regions.drop(['RegionSite', 'RegionNorth', 'RegionSouth', 'RegionEast', 'RegionWest'], axis=1)
-df_Location_Regions = pandas.merge(df_Location, df_Regions_clean, left_on='Region', right_on='RegionId', how='left')
-print(df_Location.shape[0] == df_Location_Regions.shape[0])
+df_Location_Regions = pd.merge(df_Location, df_Regions_clean, left_on='Region', right_on='RegionId', how='left')
+# print(df_Location.shape[0] == df_Location_Regions.shape[0])
 
 
 ### Crawls df
 df_Crawl_clean = df_Crawl.drop(['RegionalCrawlID'], axis=1)
 df_Specie_clean = df_Specie.drop(['SpeciePic'], axis=1)
-df_Crawl_Specie = pandas.merge(df_Crawl_clean, df_Specie_clean, left_on='SpecieID', right_on='SpecieId',how='left')
-df_Crawl_Specie_Location = pandas.merge(df_Crawl_Specie, df_Location_Regions, left_on= 'Location', right_on='LocationID', how='left')
-print(df_Crawl.shape[0] == df_Crawl_Specie_Location.shape[0])
+df_Crawl_Specie = pd.merge(df_Crawl_clean, df_Specie_clean, left_on='SpecieID', right_on='SpecieId',how='left')
+df_Crawl_Specie_Location = pd.merge(df_Crawl_Specie, df_Location_Regions, left_on= 'Location', right_on='LocationID', how='left')
+# print(df_Crawl.shape[0] == df_Crawl_Specie_Location.shape[0])
+
 # df_CrawlContact.shape[0]==df_CrawlContact.CrawlID.nunique()
 
-### check for nan FullNames:
-for n in range(0,len(df_Contacts_Position_Organization)):
-    print(df_Contacts_Position_Organization['FullName'][n],df_Contacts_Position_Organization['ContactId'][n])
 
 
-## להוסיף לפי Crawl id  את שם המדווח - אם יש כמה שמות- את הסטרינג שלהם, אם לא- אז את סטרינג המקור של שם המדווח
-
-def GetContactStr(crawlcontact_id):
-    # iloc_contact_id = np.where(df_Contacts_Position_Organization['ContactId'] == crawlcontact_id)
-    # contact = df_Contacts_Position_Organization.iloc[iloc_contact_id[0]]
+def GetContact(crawlcontact_id):
     contact = df_Contacts_Position_Organization.loc[df_Contacts_Position_Organization['ContactId'] == crawlcontact_id]
-    contact_name = contact['FullName'].item()
-    return contact_name
+    # contact_name = contact['FullName'].item()
+    return contact
 
 
-df_Crawl_Specie_Location_Contacts=df_Crawl_Specie_Location
-df_Crawl_Specie_Location_Contacts['observers_names'] = ''
+df_Crawl_Specie_Location_Contacts = df_Crawl_Specie_Location
+df_Crawl_Specie_Location_Contacts['main_observer_name'] = ''
+df_Crawl_Specie_Location_Contacts['other_observers_names'] = ''
 
 
-crawl_ids_counter = Counter(df_CrawlContact.CrawlID)
 crawl_ids_counter = Counter(df_CrawlContact.CrawlID)
 
 for crawl_id in crawl_ids_counter.keys():
-    # ilocs_of_crawl = np.where(df_CrawlContact['CrawlID'] == crawl_id)[0]
-    # contacts_ids=df_CrawlContact.iloc[ilocs_of_crawl]['ContactID']
-    # print("a",contacts_ids)
-
+    more_contacts_str =''
     contacts_ids=df_CrawlContact.loc[df_CrawlContact.CrawlID == crawl_id]
     contact_list = []
+
+    ### fixme from here-
+    # if len(contacts_ids)>1:
+    #     print(contacts_ids)
+    #     c=contacts_ids
+    #
+    #     contacts = df_Contacts_Position_Organization[df_Contacts_Position_Organization['ContactId'].isin(c['ContactID'])]##fixme contine here-- to bring more columns about main observer.
+
     for crawlcontact_id in contacts_ids['ContactID']:
-        contact_name = GetContactStr(crawlcontact_id)
+        contact = df_Contacts_Position_Organization.loc[df_Contacts_Position_Organization['ContactId'] == crawlcontact_id]
+        contact_name = contact['FullName'].item()
         contact_list.append(contact_name)
-    contacts_str = ', '.join(contact_list)
+    main_contact_name = contact_list[0]
+    if len(contact_list)>0:
+        more_contacts_str = ', '.join(contact_list[1:])
+    print('crawl_id: ',crawl_id)
+    print('main_contac: ',main_contact_name)
+    print('more_contacts_str: ',more_contacts_str)
 
-    iloc_crawl_id = np.where(df_Crawl_Specie_Location_Contacts['CrawlID'] == crawl_id)
-    print(crawl_id,iloc_crawl_id, contacts_str)
-        # contact = df_Crawl_Specie_Location_Contacts.iloc[iloc_contact_id[0]]
+    updating_index = np.where(df_Crawl_Specie_Location_Contacts['CrawlID']==crawl_id)[0][0]
+    df_Crawl_Specie_Location_Contacts.at[updating_index,'other_observers_names'] = more_contacts_str
+    df_Crawl_Specie_Location_Contacts.at[updating_index,'main_observer_name'] = main_contact_name
+    # df_Crawl_Specie_Location_Contacts.at[updating_index,'main_observer_name'] = main_contact
+    df_Crawl_Specie_Location_Contacts.at[updating_index, 'CrawlID']
+
+    print('new CrawlID: ', df_Crawl_Specie_Location_Contacts.at[updating_index, 'CrawlID'])
+    print('new main_observer_name: ',df_Crawl_Specie_Location_Contacts.at[updating_index, 'main_observer_name'])
+    print('new other_observers_names: ',df_Crawl_Specie_Location_Contacts.at[updating_index, 'other_observers_names'])
+    print('-------------------------')
+    print('')
+
+
+##inspect data
+df_Crawl_Specie_Location_Contacts.columns ## which columns there are
+df_Crawl_Specie_Location_Contacts.Nest.unique() ## what values this col contains
 
 
 
 
 
 
-# pandas.merge(df_ClutchToNest,df_Nest,)
-# a= pandas.merge(df_Nest)
-# b=pandas.merge(a,df_Clutches)
 
 
-# xlwriter = pandas.ExcelWriter('turtles_df_excel_try3.xlsx')
+
+
+
+
+# pd.merge(df_ClutchToNest,df_Nest,)
+# a= pd.merge(df_Nest)
+# b=pd.merge(a,df_Clutches)
+
+
+# xlwriter = pd.ExcelWriter('turtles_df_excel_try3.xlsx')
 # df_Location_Regions.to_excel(xlwriter, sheet_name='a', index=False)
 # xlwriter.close()
 
@@ -160,7 +173,7 @@ for crawl_id in crawl_ids_counter.keys():
 
 
 
-# xlwriter = pandas.ExcelWriter('turtles_df_excel.xlsx')
+# xlwriter = pd.ExcelWriter('turtles_df_excel.xlsx')
 # df_AcCrawl.to_excel(xlwriter, sheet_name='AcCrawl', index=False)
 # df_AcHatch.to_excel(xlwriter, sheet_name='AcHatch', index=False)
 # df_Activities.to_excel(xlwriter, sheet_name='Activities', index=False)
